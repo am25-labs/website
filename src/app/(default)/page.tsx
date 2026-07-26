2bfb9812e40faec4df4b12ca9bdb0a18b2862428
martirale <hola@alemartir.com>
2026-07-26 14:20:47 -0600

    Add Featured Works

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/home/Hero";
+import FeaturedWork from "@/components/home/FeaturedWork";
 import RecentEntries from "@/components/home/RecentEntries";
 import Services from "@/components/home/Services";
 import { getHome } from "@/lib/plank/fetch";
@@ -10,6 +11,7 @@ export default async function HomePage() {
   return (
     <>
       <Hero heading={heading} description={description} />
+      <FeaturedWork />
       <Services services={services} />
       <AuditBanner />
       <RecentEntries />
